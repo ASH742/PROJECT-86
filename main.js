62534060f0549bd00951851794a37f3529d2bf5f
ASH742 <76694155+ASH742@users.noreply.github.com>
2021-10-04 20:00:03 +0530

    Update main.js

--- main.js
+++ main.js
@@ -6,8 +6,8 @@ function new_image()
 	fabric.Image.fromURL("BirthdayImage.jpg",function(Img){
 		block_image_object=Img;
 
-		block_image_object.scaleToWidth(700);
-		block_image_object.scaleToHeight(150);
+		block_image_object.scaleToWidth(600);
+		block_image_object.scaleToHeight(400);
 		block_image_object.set({
 			top:0,
 			left:0
